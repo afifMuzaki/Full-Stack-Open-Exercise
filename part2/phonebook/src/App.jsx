bbfd142aeaaeb53c4d3ed9b51181bc64658cb009
afifMuzaki <afifmuzaki943@gmail.com>
2024-02-03 12:05:00 +0700

    part2/exer_2.11. phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
 import Filter from './Filter';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data);
+    })
+    .catch(err => {
+      alert('Error: unable to connect to server');
+      console.error(err.message);
+    });
+  }, []);
 
   const shownPerson = filter ? persons.filter(person => 
     person.name.toLowerCase().includes(filter.toLowerCase())
